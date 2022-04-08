2f8149dc0031ae445aa5255256cd5fcef241e760
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2022-04-08 23:38:51 +0200

    feat(filterFuncionality): initial filter component funcionality

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Filters.scss";
 
 export const Filters = () => {
-  return <div>Filters</div>;
+  const [filtersVisibitity, setFiltersVisibitity] = useState(false);
+  const [avaliable, setAvaliable] = useState(false);
+  const [batteryLevel, setBatteryLevel] = useState(20);
+  const showFilters = () => {
+    setFiltersVisibitity(!filtersVisibitity);
+  };
+
+  useEffect(() => {
+    console.log(avaliable);
+    console.log(batteryLevel);
+  }, [batteryLevel, avaliable]);
+
+  return (
+    <section className="filter__container">
+      <button
+        className={`filter__container--btn ${filtersVisibitity && "active"}`}
+        onClick={showFilters}
+      >
+        Show filters
+      </button>
+      {filtersVisibitity ? (
+        <div className="filter__container--filters">
+          <span>
+            Only avaliable
+            <input
+              name="avaliability"
+              type="checkbox"
+              checked={avaliable}
+              onChange={() => setAvaliable(!avaliable)}
+            />
+          </span>
+
+          <span className="range">
+            Minimum battery {batteryLevel}%
+            <input
+              name="battery"
+              type="range"
+              defaultValue={batteryLevel}
+              onChange={(e) => setBatteryLevel(e.target.value)}
+            />
+          </span>
+        </div>
+      ) : null}
+    </section>
+  );
 };
